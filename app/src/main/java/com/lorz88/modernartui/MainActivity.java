fa56511ab0c67a04b88e0ae5eb9b1fca363f7146
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2015-03-22 15:07:15 +0100

    More info dialog working

--- app/src/main/java/com/lorz88/modernartui/MainActivity.java
+++ app/src/main/java/com/lorz88/modernartui/MainActivity.java
@@ -1,12 +1,20 @@
 package com.lorz88.modernartui;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int MENU_MORE_INFO = Menu.FIRST;
+    private static final String TAG = "ModernArtUI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(Menu.NONE, MENU_MORE_INFO, Menu.NONE, "More information");
         return true;
     }
 
@@ -30,10 +39,39 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == MENU_MORE_INFO) {
+                createDialog();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MoreInfoDialog extends DialogFragment {
+
+        public static MoreInfoDialog newInstance() {
+            return new MoreInfoDialog();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.opts_dialog_message)
+                    .setPositiveButton(R.string.opts_dialog_visit_moma, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.d(TAG, "On Click for Create Dialog");
+                        }
+                    })
+                    .setNegativeButton(R.string.opts_dialog_not_visit, null);
+            return builder.create();
+        }
+
+    }
+
+    private void createDialog() {
+        DialogFragment dialogFragment = new MoreInfoDialog();
+        dialogFragment.show(getFragmentManager(), "More Info");
+
     }
 }
