af274f59b73ba2ee537a501f3e146790033c3b5e
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2015-03-23 01:31:28 +0100

    Seek bar produces expected changes

--- app/src/main/java/com/lorz88/modernartui/MainActivity.java
+++ app/src/main/java/com/lorz88/modernartui/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -46,6 +48,7 @@ public class MainActivity extends ActionBarActivity {
                                     tv31, tv32, tv33, tv34, tv35,
                                     tv41, tv42, tv43, tv44, tv45,
                                     tv51, tv52, tv53, tv54, tv55};
+    private int [] mOriginalColors = new int[mTVArray.length];
     private SeekBar mSeekbar;
 
     @Override
@@ -63,26 +66,40 @@ public class MainActivity extends ActionBarActivity {
             //Iterate throught the TextViews
             View childLinLayout = ((ViewGroup)mainLayout).getChildAt(i);
             for (int t = 0; t < ((ViewGroup)childLinLayout).getChildCount(); t++) {
-                Log.d(TAG, ">> Iterating now throuw Layouts t: "+t);
+                Log.d(TAG, ">> Iterating now throuw Layouts t: "+t+"ArrayPos: "+((int)i+(int)t));
                 TextView textView = (TextView) ((ViewGroup) childLinLayout).getChildAt(t);
-                mTVArray[i+t] = textView;
+                mTVArray[i*5+t] = textView;
+
+                int color = ((ColorDrawable) textView.getBackground()).getColor();
+                mOriginalColors[i*5+t] = color;
             }
         }
 
         mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                
+
+                for (int i= 0; i < mTVArray.length; i++) {
+                    int color;
+                    if ((i+1)%5 == 0 && i !=0 && i != mTVArray.length-1) {
+                        color = transitionColor(mOriginalColors[i], mOriginalColors[(i+3)%mTVArray.length], (float) progress/30);
+                    }
+                    else if (i ==  mTVArray.length-1) {
+                        color = transitionColor(mOriginalColors[i], mOriginalColors[(i+2)%mTVArray.length], (float) progress/30);
+                    }
+                    else {
+                        color = transitionColor(mOriginalColors[i], mOriginalColors[(i + 1) % mTVArray.length], (float) progress /30);
+                    }
+                    mTVArray[i].setBackgroundColor(color);
+                }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
@@ -137,5 +154,12 @@ public class MainActivity extends ActionBarActivity {
         DialogFragment dialogFragment = new MoreInfoDialog();
         dialogFragment.show(getFragmentManager(), "More Info");
 
+    }
+
+    private static int transitionColor(int startColor, int finalColor, float percentage) {
+        int transitionRed = (int) Color.red(startColor) + Math.round((Color.red(finalColor)-Color.red(startColor)) * (float) percentage);
+        int transitionGreen = (int) Color.green(startColor) + Math.round((Color.green(finalColor)-Color.green(startColor)) * (float) percentage);
+        int transitionBlue = (int) Color.blue(startColor) + Math.round((Color.blue(finalColor)-Color.blue(startColor)) * (float) percentage);
+        return Color.argb(Color.alpha(startColor), transitionRed, transitionGreen, transitionBlue);
     }
 }
